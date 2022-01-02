f211ca935edf6aee193209216e732f11904a3b69
pratikal1 <chandpratikal1@gmail.com>
2022-01-02 23:49:44 +0530

    Removed redundant code and setup project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Home } from './components';
+
+import { useBlogsContext } from './context/blogscontext';
 
 function App() {
+  const { data, loading, error } = useBlogsContext();
+  if (loading) return <div>Loading</div>;
+  if (error) return <div>Error</div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Home />
     </div>
   );
 }
---
+++ src/context/blogscontext.js
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const BlogsContext = createContext();
+
+export function BlogsContextProvider({ children }) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const getData = () => {
+    setLoading(true);
+    fetch('data.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        setData(myJson);
+        setLoading(false);
+      })
+      .catch(err => setError(err));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const value = { data, loading, error };
+  return (
+    <BlogsContext.Provider value={value}>{children}</BlogsContext.Provider>
+  );
+}
+
+export function useBlogsContext() {
+  return useContext(BlogsContext);
+}
